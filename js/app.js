dba00ecd2e8840af04d75368a2997f578548a2f5
Jake Avery <jakeavery@jakeavery.me>
2016-12-11 16:35:16 -0500

    WOOHOO! Resume cards equalized on small through large screens. DOPE!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation();
+
+// Equalized Resume section width at large screens
+$(document).ready(function() {
+    // Run on initial page load
+    equalizeResume();
+    
+    // Run on resize of the window
+    $(window).resize(equalizeResume);
+});
+
+function equalizeResume() {
+    // Check if Skills and Tech cards are stacked or not. If not, equalize!
+    if ($('#right-card').offset().left - $('#left-card').offset().left - $('#left-card').outerWidth() > 0) {
+        // Space between left and right cards
+        var spaceBetween = $('#right-card').offset().left - $('#left-card').offset().left - $('#left-card').outerWidth();
+        // Total width of cards + space between
+        var equalizedWidth = $('#left-card').outerWidth() + $('#right-card').outerWidth() + spaceBetween+"px";
+        // Set Resume cards to equalized width
+        $('#work-leadership .card, #education .card').css("max-width", equalizedWidth);
+        //$('#education .card').css("max-width", equalizedWidth);
+    }
+}
