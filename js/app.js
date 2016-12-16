556e6c6e5fc2bff6436b7a59f80fd1819d42599b
Jake Avery <jakeavery@jakeavery.me>
2016-12-15 19:14:12 -0500

    Skills & Tech cards positioned correctly across all screen sizes!

--- js/app.js
+++ js/app.js
@@ -29,6 +29,16 @@ function equalizeCards() {
         }
     } else if ($("#large-cta-button .show-for-large").is(":visible") == true ) {
         // xlarge and up
+        // Position Skills & Leadership cards correctly
+        var leftMargin = $("#work-leadership .card").offset().left;
+        console.log("Left margin: "+leftMargin);
+        var viewWidth = $(window).width();
+        console.log("Viewport width: "+viewWidth);
+        var cardWidth = $("#skills-tech .card").outerWidth();
+        var cardOffset = viewWidth - leftMargin - cardWidth;
+        console.log(cardOffset);
+        $("#skills-tech .card").offset({left: cardOffset});
+        // Equalize and position Showcase cards correctly
     }
     
 }
